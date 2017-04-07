0fa930de5cfd04ef38bb9f772b0d29ed4f963de2
Benjamin <brskl@msn.com>
2017-04-06 19:03:25 -0700

    initial checkin. TODO: Move buttons inside <form>

---
+++ hw01/number.js
@@ -0,0 +1,44 @@
+var numbers = [];
+
+function submit(){
+  event.preventDefault();
+  var newValue = Number(document.numberForm.num.value);
+  numbers.push(newValue);
+  updateFields();
+  document.numberForm.num.value = "";
+}
+
+function reset() {
+  event.preventDefault();
+  numbers = [];
+  updateFields();
+}
+
+function updateFields()
+{
+  var divCount = document.getElementById("count");
+  divCount.innerHTML = "Count: " + numbers.length;
+  var divSum = document.getElementById("sum");
+  var divAvg = document.getElementById("average")
+  if (numbers.length == 0) {
+    divSum.innerHTML = "Sum: 0";
+    divAvg.innerHTML = "Average: 0";
+  } else {
+    var total = 0;
+    var value;
+    for (value of numbers) {
+      total += value;
+    }
+    divSum.innerHTML = "Sum: " + total;
+    divAvg.innerHTML = "Average: " + (total / numbers.length);
+  }
+}
+
+function init()
+{
+  reset();
+  updateFields();
+  
+}
+
+window.onload = init;
