a9d6d99dacb8c737f223ca4dcc0f99ad5d3bbac5
brsklar <brskl@msn.com>
2017-04-27 19:52:13 -0700

    added checkbox

--- hw03/input.js
+++ hw03/input.js
@@ -1,8 +1,16 @@
 
+function onCB(evt) {
+  var cbVal = this.checked;
+  $('#cb1').prop('checked', cbVal);
+  $('#cb2').prop('checked', cbVal);
+  $('#cbValue').text(cbVal.toString());
+}
 
 function init()
 {
-  
+  $('#cb1').on( 'click', onCB );
+  $('#cb2').on( 'click', onCB );
+  $('#cbValue').text($('#cb1').prop('checked').toString());
 }
 
 window.onload = init;
