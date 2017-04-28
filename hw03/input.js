d8830f847106abe31c7da7676da4608f879ce8da
brsklar <brskl@msn.com>
2017-04-27 20:23:13 -0700

    added color

--- hw03/input.js
+++ hw03/input.js
@@ -6,11 +6,21 @@ function onCB(evt) {
   $('#cbValue').text(cbVal.toString());
 }
 
+function onColor(evt) {
+  var colorVal = this.value;
+  $('#color1').prop('value', colorVal);
+  $('#color2').prop('value', colorVal);
+  $('#colorValue').text(colorVal.toString());
+}
+
 function init()
 {
-  $('#cb1').on( 'click', onCB );
-  $('#cb2').on( 'click', onCB );
+  $('#cb1').on('click', onCB);
+  $('#cb2').on('click', onCB);
   $('#cbValue').text($('#cb1').prop('checked').toString());
+  $('#color1').on('change', onColor);
+  $('#color2').on('change', onColor);
+  $('#colorValue').text($('#color1').prop('value').toString());
 }
 
 window.onload = init;
