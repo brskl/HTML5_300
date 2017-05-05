600efe05a6eeb96e33dc2e08eec1b7f876ec55bb
brsklar <brskl@msn.com>
2017-05-05 10:44:10 -0700

    minor adj. of lat/long coords

--- hw04/maps.js
+++ hw04/maps.js
@@ -15,33 +15,27 @@
     var sizeUrl= 'size=' + $('#staticmapimg').attr('width') + 'x' + $('#staticmapimg').attr('height'); 
     switch(this.value) {
       case 'Yosemite':
-        // https://www.google.com/maps/@37.8529771,-119.8313059,10z?hl=en
-        centerUrl='center=37.8529771,-119.8313059';
+        centerUrl='center=37.8486397,-119.5593908';
         zoomUrl='zoom=10';
         break;
       case 'Yellowstone':
-        // https://www.google.com/maps/@44.5844247,-111.0745171,9z?hl=en
-        centerUrl='center=44.5844247,-111.0745171';
+        centerUrl='center=44.3884794,-110.5526539';
         zoomUrl='zoom=9';
         break;
       case 'GrandCanyon':
-        // https://www.google.com/maps/@36.0871859,-113.9644705,8z?hl=en
-        centerUrl='center=36.0871859,-113.9644705';
-        zoomUrl='zoom=8';
+        centerUrl='center=36.0600259,-112.0797528';
+        zoomUrl='zoom=9';
         break;
       case 'Zion':
-        //https://www.google.com/maps/@37.3217618,-113.3234313,10z?hl=en
-        centerUrl='center=37.3217618,-113.3234313';
+        centerUrl='center=37.2944544,-113.0336634';
         zoomUrl='zoom=10';
         break;
       case 'Olympic':
-        // https://www.google.com/maps/@47.7844309,-124.200644,9z?hl=en
-        centerUrl='center=47.7844309,-124.200644';
+        centerUrl='center=47.8139504,-123.6238498';
         zoomUrl='zoom=9';
         break;
       case 'Rainer':
-        // https://www.google.com/maps/@46.8597651,-121.995113,10z?hl=en
-        centerUrl='center=46.8597651,-121.995113';
+        centerUrl='center=46.8656554,-121.7286653';
         zoomUrl='zoom=10';
         break;
       default:
