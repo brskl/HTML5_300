5a93faa59d92a512ad14c131d5b0524738c79756
brsklar <brskl@msn.com>
2017-05-05 11:37:57 -0700

    added base code for leaftlet map. TODO: set value in onSubmit function

--- hw04/maps.js
+++ hw04/maps.js
@@ -51,6 +51,16 @@
 
   //=============================================================================
 
+  var leafletMap = L.map('slippymapdiv');
+  // add attribution
+  L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+        attribution: '© <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        maxzoom: 19
+    } ).addTo( leafletMap );
+  // set default to Seattle, WA
+  leafletMap.setView([47.6018006,-122.3386613], 12);
+
   $('#latlong').submit(onLatLongSubmit);
 
   function onLatLongSubmit(evt) {
