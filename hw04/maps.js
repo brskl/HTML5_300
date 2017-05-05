e9bb87ae8f8217e6c5ec3b311f81adcb8cda87c3
brsklar <brskl@msn.com>
2017-05-04 20:50:32 -0700

    added static map. Need to update lat/long/zoom a bit.

--- hw04/maps.js
+++ hw04/maps.js
@@ -1,1 +1,56 @@
 
+(function() {
+'use strict';
+  //=============================================================================
+  $('#selectMap').on('change', onListChange);
+
+  var googleStaticMapsApiKey = 'AIzaSyCh3xLQZF7RK22zO7n79jXtmDL0w4xmk1w';
+
+  //<img src="https://maps.googleapis.com/maps/api/staticmap?size=600x400&center=47.6205,-122.3493&zoom=16&markers=color:yellow|47.6205,-122.3493&key=AIzaSyDU6lWXAex1uw3neucrHf9BSGRExTkwqxQ" />
+
+  function onListChange(evt) {
+    var mapUrl='#';
+    var centerUrl;
+    var zoomUrl;
+    switch(this.value) {
+      case 'Yosemite':
+        // https://www.google.com/maps/@37.8529771,-119.8313059,10z?hl=en
+        centerUrl='center=37.8529771,-119.8313059';
+        zoomUrl='zoom=10';
+        break;
+      case 'Yellowstone':
+        // https://www.google.com/maps/@44.5844247,-111.0745171,9z?hl=en
+        centerUrl='center=44.5844247,-111.0745171';
+        zoomUrl='zoom=9';
+        break;
+      case 'GrandCanyon':
+        // https://www.google.com/maps/@36.0871859,-113.9644705,8z?hl=en
+        centerUrl='center=36.0871859,-113.9644705';
+        zoomUrl='zoom=8';
+        break;
+      case 'Zion':
+        //https://www.google.com/maps/@37.3217618,-113.3234313,10z?hl=en
+        centerUrl='center=37.3217618,-113.3234313';
+        zoomUrl='zoom=10';
+        break;
+      case 'Olympic':
+        // https://www.google.com/maps/@47.7844309,-124.200644,9z?hl=en
+        centerUrl='center=47.7844309,-124.200644';
+        zoomUrl='zoom=9';
+        break;
+      case 'Rainer':
+        // https://www.google.com/maps/@46.8597651,-121.995113,10z?hl=en
+        centerUrl='center=46.8597651,-121.995113';
+        zoomUrl='zoom=10';
+        break;
+      default:
+        return;
+    }
+
+    mapUrl='https://maps.googleapis.com/maps/api/staticmap?size=600x400&' + centerUrl + '&' + zoomUrl + '&key' + googleStaticMapsApiKey;
+
+    $('#staticmapimg').attr('src', mapUrl);
+  }
+
+  //=============================================================================
+})();
