dae895c280fd595d84f84bfb4bef56f19df56439
brsklar <brskl@msn.com>
2017-05-11 18:08:37 -0700

    added function skeleton for 'add person' button and 'Submit' button

--- hw05/list.js
+++ hw05/list.js
@@ -1,11 +1,27 @@
 
 (function() {
 'use strict';
-
   $('#dataForm').css('display', 'none');
+  $('#addPerson').on('click', onAddPerson);
+  $('#formPerson').submit(onSubmitPerson);
+
+
   //=============================================================================
+
+  function onAddPerson(evt) {
+    $('#dataForm').css('display', 'initial');
+    $('#sectionTable').css('display', 'none');
+  }
+
+  function onSubmitPerson(evt) {
+    evt.preventDefault();
+    $('#dataForm').css('display', 'none');
+    $('#sectionTable').css('display', 'initial');
   
+  }
+
   //=============================================================================
+
 
   //=============================================================================
 })();
