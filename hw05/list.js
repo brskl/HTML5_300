d7fed23423ad0b9a30eeeb67861f0e0ab4ef7f82
brsklar <brskl@msn.com>
2017-05-12 21:31:33 -0700

    added edit button and functionality

--- hw05/list.js
+++ hw05/list.js
@@ -2,6 +2,8 @@
 (function() {
 'use strict';
   var people=[];
+  var isEdit;
+  var indexEdit;
 
   $('#sectionForm').hide();
   $('#addPerson').on('click', onAddPerson);
@@ -14,31 +16,49 @@
     $('#sectionForm').show();
     $('#sectionTable').hide();
     clearForm();
+    isEdit = false;
   }
 
   function onSubmitPerson(evt) {
     evt.preventDefault();
-    $('#sectionForm').css('display', 'none');
-    $('#sectionTable').css('display', 'initial');
+    $('#sectionForm').hide();
+    $('#sectionTable').show();
 
-    var person = new Object;
-    person.name = this.name.value;
-    person.address = this.address.value;
-    person.email = this.email.value;
+    if (isEdit) {
+      people[indexEdit].name = this.name.value;
+      people[indexEdit].address = this.address.value;
+      people[indexEdit].email = this.email.value;
+    } else {
+      var person = new Object;
+      person.name = this.name.value;
+      person.address = this.address.value;
+      person.email = this.email.value;
+      people.push(person);
+    }
 
-    people.push(person);
     updateTable();
   }
 
   function onCancelPerson(evt) {
-    $('#sectionForm').css('display', 'none');
-    $('#sectionTable').css('display', 'initial');
+    $('#sectionForm').hide();
+    $('#sectionTable').show();
   }
 
   function clearForm() {
     $('#formPerson').each(function(){
       this.reset();
     });
+  }
+
+  function editPerson(i) {
+    isEdit = true;
+    indexEdit = i;
+    $('#sectionForm').show();
+    $('#sectionTable').hide();
+    var personForm = $('#formPerson')[0];
+    personForm.name.value = people[i].name;
+    personForm.address.value = people[i].address;
+    personForm.email.value = people[i].email;
   }
 
   function updateTable() {
@@ -57,6 +77,11 @@
       newTd.text(obj.email);
       newTr.append(newTd);
       newTd = $('<td>');
+      newButton = $('<button>').text('Edit');
+      newButton.click(function() {
+        editPerson(i);
+      });
+      newTd.append(newButton);
       newButton = $('<button>').text('Delete');
       newButton.click(function() {
         people.splice(i,1);
