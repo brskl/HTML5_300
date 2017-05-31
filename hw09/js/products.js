caf72774c1a7b7e43f65367c844fc90886869ffb
brsklar <brskl@msn.com>
2017-05-30 17:20:36 -0700

    copied from HTML5 200B e-comm website assignment

---
+++ hw09/js/products.js
@@ -0,0 +1,204 @@
+var products = [
+  // Gloves
+  {
+    // from http://www.zappos.com
+    "name": "Black Diamond HeavyWeight Mitten",
+    "price": 24.99,
+    "description": "Stay warm in even the toughest of conditions with this HeavyWeight Mitt from Black Diamond™. Designed for skiing, hiking or climbing in cold conditions. Soft, stretchy and breathable Polartec® Power Stretch® shell. Grippy and durable goat leather palm with Kevlar® stitching.",
+    "imageTitle": "mitten-black-diamond.jpg",
+    "imageSet":  "",
+    "imageSizes": "",
+    "category": "mitten",
+    "productType": "Gloves"
+  },
+
+  {
+    // from http://www.zappos.com
+    "name": "Columbia Mighty Lite™ Glove",
+    "price": 30.99,
+    "description": "Columbia® keeps your hands warm, without sacrificing dexterity, in the Mighty Lite™ Glove. Lightweight, abrasion-resistant glove. Triple-ripstop shell material. Omni-Shield® treatment is stain repellant.",
+    "imageTitle": "columbia-mighty-lite.jpg",
+    "imageSet":  "",
+    "imageSizes": "",
+    "category": "fullfinger",
+    "productType": "Gloves"
+  },
+
+  {
+    // from http://www.zappos.com
+    "name": "Goorin Brothers Pier ",
+    "price": 17.99,
+    "description": "Ahoy matey! Go from ship to shore in style with the Goorin Brothers® Pier gloves! Fingerless gloves with ribbed cuffs. Brand logo hardware pin. 100% cotton. Imported.",
+    "imageTitle": "goorin-brothers-pier.jpg",
+    "imageSet":  "",
+    "imageSizes": "",
+    "category": "fingerless",
+    "productType": "Gloves"
+  },
+
+  // Hats
+  {
+    // from http://www.villagehatshop.com
+    "name": "Seagrass Straw Ascot Cap",
+    "price": 33.99,
+    "description": "Incredibly breathable and lightweight, the Seagrass Straw Ascot Cap by Capas Headwear is unique among straw caps. Featuring a loose-knit seagrass straw build, this ascot provides shading from the sun while maximizing ventilation. Natural seagrass possesses wonderful color variation that gives the cap a uniquely organic and relaxed look.",
+    "imageTitle": "seagrass-straw-ascot.jpg",
+    "imageSet":  "",
+    "imageSizes": "",
+    "category": "ascot",
+    "productType": "Hats"
+  },
+
+  {
+    // from http://www.villagehatshop.com
+    "name": "Los Angeles Dodgers MLB Rockford Mesh Fitted Baseball Cap",
+    "price": 27.99,
+    "description": "Show your team pride in relaxed comfort. The 47 Brand Los Angeles Dodgers MLB Rockford Mesh Baseball Cap features the team logo in raised embroidery on the two front panels and breathable, soft mesh on the remaining four panels. Finished with the team name embroidered on the back and a stretchy cotton elastic sweatband, the 47 Brand MLB Rockford Mesh Baseball Cap is bound to be your new favorite baseball cap!",
+    "imageTitle": "la-dodgers-mesh.jpg",
+    "imageSet":  "",
+    "imageSizes": "",
+    "category": "baseball",
+    "productType": "Hats"
+  },
+
+  {
+    // from http://www.villagehatshop.com
+    "name": "Seattle Mariners MLB Woodside Stripe Snapback Baseball Cap",
+    "price": 29.99,
+    "description": "Rock some retro stripes with the Seattle Mariners MLB Woodside Stripe Snapback Baseball Cap from 47 Brand! Classically designed with six-panel construction with sewn eyelets, top button, adjustable snapback and a sleek, flat brim - the 47 Brand Woodside Stripe Snapback Baseball Cap is an ode to a bygone MLB era. Prominently featuring raised embroidery of the Seattle Mariners MLB logo on the front and their MLB division logo on the side, this is a uniquely styled baseball cap sure to turn heads!",
+    "imageTitle": "seattle-mariners-stripe.jpg",
+    "imageSet":  "",
+    "imageSizes": "",
+    "category": "baseball",
+    "productType": "Hats"
+  },
+
+  {
+    // from http://www.villagehatshop.com
+    "name": "Classic Cuff Wool Blend Beanie Hat",
+    "price": 21.00,
+    "description": "Crafted in the \"skull cap\" beanie hat tradition, Jaxon Hats' Classic Wool Blend Cuff Beanie Hat features an alternating cable and wide rib knit patterned crown with a contrasting wide rib knit cuff. Made of an equal parts wool and acrylic blend, the Classic Wool Blend Cuff Beanie Hat is soft and warm. Its stylish cuff can be worn folded up (as is) or down for a longer, slouchy beanie hat look.",
+    "imageTitle": "classic-cuff-wool-beanie.jpg",
+    "imageSet":  "",
+    "imageSizes": "",
+    "category": "beanie",
+    "productType": "Hats"
+  },
+
+  {
+    // from http://www.villagehatshop.com
+    "name": "Wool Fashion Beret",
+    "price": 14.95,
+    "description": "Made of 100% Wool, Village Hat Shop brings you the classically timeless Wool Fashion Beret in a wide assortment of colors. Sleek and simple, this seamless wool beret is topped with a small tonal tassel. Wear it tilted to either side of your head, centered or towards the back - the Wool Fashion Beret adds a touch of effortless French flair to your look!",
+    "imageTitle": "wool-fashion-beret.jpg",
+    "imageSet":  "",
+    "imageSizes": "",
+    "category": "beret",
+    "productType": "Hats"
+  },
+
+  // Scarves
+  {
+    "name": "Reversible Plaid",
+    "price": 26.99,
+    "description": "Two classic patterns in one great look: This supersoft and cozy reversible scarf instantly doubles your street-style cred. 100% acrylic.",
+    "imageTitle": "reversible-plaid.jpg",
+    "imageSet":  "images/reversible-plaid-200w.jpg 200w, images/reversible-plaid.jpg 380w",
+    "imageSizes": "(max-width: 400px) 200px, 380px",
+    "category": "plaid",
+    "productType": "Scarves"
+  },
+  {
+    "name": "Wool Cable Knit",
+    "price": 49.99,
+    "description": "Warm yourself with this women's natural cable knit scarf, crafted from 100% Merino wool. Imported.",
+    "imageTitle": "wool-cable.jpeg",
+    "imageSet": "images/wool-cable-200w.jpeg 200w, images/wool-cable.jpeg 300w",
+    "imageSizes": "(max-width: 400px) 200px, 300px",
+    "category": "solid",
+    "productType": "Scarves"
+  },
+  {
+    "name": "Northern Lights",
+    "price": 29.99,
+    "description": "Handmade by women in Agra, sales provide medical and educational support in this remote area of India. Crinkly 100% cotton.",
+    "imageTitle": "northern-lights.jpg",
+    "imageSet": "images/northern-lights-200w.jpg 200w, images/northern-lights-400w.jpg 400w, images/northern-lights.jpg 600w",
+    "imageSizes": "(max-width: 400px) 200px, (max-width: 800px) 400px, 600px",
+    "category": "",
+    "productType": "Scarves"
+  },
+  {
+    "name": "Ombre Infinity",
+    "price": 11.99,
+    "description": "A dip-dye effect adds color and dimension to a cozy infinity scarf featuring a soft, chunky knit. 100% acrylic.",
+    "imageTitle": "ombre-infinity.jpg",
+    "imageSet": "images/ombre-infinity-200w.jpg 200w, images/ombre-infinity-400w.jpg 400w, images/ombre-infinity.jpg 860w",
+    "imageSizes": "(max-width: 400px) 200px, (max-width: 800px) 400px, 860px",
+    "category": "solid",
+    "productType": "Scarves"
+  },
+  {
+    "name": "Fringed Plaid",
+    "price": 18.99,
+    "description": "Generously sized, extra soft and featuring a dazzling fringe, this scarf is rendered in a versatile gray, black and white plaid. Expertly beat the cold with style. 100% acrylic.",
+    "imageTitle": "fringed-plaid.jpeg",
+    "imageSet":  "images/fringed-plaid-200w.jpeg 200w, images/fringed-plaid-400w.jpeg 400w, images/fringed-plaid-800w.jpeg 800w, images/fringed-plaid.jpeg 960w",
+    "imageSizes": "(max-width: 400px) 200px, (max-width: 800px) 400px, (max-width: 1600px) 800px, (min-width: 1601px) 960px",
+    "category": "plaid",
+    "productType": "Scarves"
+  },
+  {
+    "name": "Multi Color",
+    "price": 22.99,
+    "description": "The Who What Wear Oversize Color-Block Square Scarf is big, bold, and designed to twist and wrap any way you wish. All the colors of the season are harmonized in this oversize accent, so you can adjust to contrast or match your outfit; soft and lush, it’s your stylish standoff against cold AC and unexpected fall breezes. 100% acrylic",
+    "imageTitle": "multi-color.jpeg",
+    "imageSet": "images/multi-color-200w.jpeg 200w, images/multi-color-400w.jpeg 400w, images/multi-color-800w.jpeg 800w, images/multi-color-1600w.jpeg 1600w, images/multi-color.jpeg 3000w",
+    "imageSizes": "(max-width: 400px) 200px, (max-width: 800px) 400px, (max-width: 1600px) 800px, (max-width: 3200px) 1600px, (min-width: 3201px) 3000px",
+    "category": "",
+    "productType": "Scarves"
+  },
+  {
+    "name": "Etro Paisley-Print Silk",
+    "price": 249.99,
+    "description": "Luxurious silk scarf with subtle paisley pattern. 100% silk",
+    "imageTitle": "etro.png",
+    "imageSet": "images/etro-200w.png 200w, images/etro.png 492w",
+    "imageSizes": "(max-width: 400px) 200px, 492px",
+    "category": "pattern",
+    "productType": "Scarves"
+  },
+  {
+    "name": "Ashby Twill",
+    "price": 70.99,
+    "description": "Faribault brings you the Ashby Twill Scarf in Natural. Woven with a 'broken' twill technique, the Ashby Twill Scarf has a slight zigzag texture. Made in USA, this timeless scarf is crafted with luxurious merino wool and finished with heather gray fringe. 100% Merino wool",
+    "imageTitle": "twill.jpg",
+    "imageSet": "images/twill-200w.jpg 200w, images/twill-400w.jpg 400w, images/twill-800w.jpg 800w, images/twill.jpg 1024w",
+    "imageSizes": "(max-width: 400px) 200px, (max-width: 800px) 400px, (max-width: 1600px) 800px, (min-width: 1601px) 1024px",
+    "category": "solid",
+    "productType": "Scarves"
+  }
+]
+
+var filterGloves = [
+  { "optionValue": "mitten", "optionText": "Mittens" },
+  { "optionValue": "fingerless", "optionText": "Fingerless" },
+  { "optionValue": "fullfinger", "optionText": "Full Finger" },
+  { "optionValue": "liner", "optionText": "Liners" },
+  { "optionValue": "convertible", "optionText": "Convertibles" }
+]
+
+var filterHats = [
+  { "optionValue": "ascot", "optionText": "Ascot" },
+  { "optionValue": "baseball", "optionText": "Baseball" },
+  { "optionValue": "beanie", "optionText": "Beanies" },
+  { "optionValue": "beret", "optionText": "Berets" },
+  { "optionValue": "dress", "optionText": "Dress" }
+]
+
+var filterScarves = [
+  { "optionValue": "stripe", "optionText": "Stripes" },
+  { "optionValue": "solid", "optionText": "Solids" },
+  { "optionValue": "plaid", "optionText": "Plaids" },
+  { "optionValue": "pattern", "optionText": "Patterns" }
+]
---
+++ hw09/js/script.js
@@ -0,0 +1,221 @@
+// products in products.js script
+
+function printProduct(product) {
+  console.log(product.name + ", " + product.description + ", " + product.price);
+}
+
+function printAllProducts() {
+  for (var i = 0; i < products.length; i++) {
+    printProduct(products[i]);
+  }
+}
+
+// create list of <option> tags from optionList data, and append to 'selectElem'
+function createOptions(selectElem, optionList) {
+  var optionElem;
+  var optionItem;
+
+  // option 'All'
+  optionElem = document.createElement("option");
+  optionElem.setAttribute("value", "");
+  optionElem.innerHTML = "All";
+  selectElem.appendChild(optionElem);
+
+  for (optionItem of optionList) {
+    optionElem = document.createElement("option");
+    optionElem.setAttribute("value", optionItem.optionValue);
+    optionElem.innerHTML = optionItem.optionText;
+    selectElem.appendChild(optionElem);
+  }
+}
+
+// populate the <select> element with the appropiate <option>s
+function fillFilterBy() {
+   if (typeof pageProductType != 'undefined') {
+    if (pageProductType.length != 0) {
+      var selectElement = document.getElementById("filterBy");
+      switch(pageProductType) {
+        case 'Gloves':
+          createOptions(selectElement, filterGloves);
+          break;
+        case 'Hats':
+          createOptions(selectElement, filterHats);
+          break;
+        case 'Scarves':
+          createOptions(selectElement, filterScarves);
+          break;
+        default:
+          // do nothing
+          break;
+      }
+    }
+  }
+}
+
+// comparison function used for sorting by name
+function compareByName(prod1, prod2)
+{
+  if (prod1.name.toLowerCase() < prod2.name.toLowerCase())
+    return -1;
+  else if (prod1.name.toLowerCase() > prod2.name.toLowerCase())
+    return 1;
+  else
+    return 0;
+}
+
+// comparison function used for sorting by price
+function compareByPrice(prod1, prod2)
+{
+  return prod1.price - prod2.price;
+}
+
+// create anew item <img> for an individual product
+function createItemImg(product) {
+  var newImg;
+  
+  newImg = document.createElement("img");
+  newImg.setAttribute("src", "images/" + product.imageTitle);
+  newImg.setAttribute("alt", product.name);
+  if (product.imageSizes) {
+    if (product.imageSizes.length > 0) {
+      newImg.setAttribute("sizes", product.imageSizes);
+      newImg.setAttribute("srcset", product.imageSet)
+    }
+  }
+  
+  return newImg;
+}
+
+// create a new item <figure> for an individual product
+function createItemFigure(product)
+{
+  var newFigure;
+  var newImg;
+  var newFigCaption;
+
+  newFigure = document.createElement("figure");
+
+  newImg = createItemImg(product);
+  newFigure.appendChild(newImg);
+
+  newFigCaption = document.createElement("figcaption");
+  newFigCaption.innerHTML = product.name;
+  newFigure.appendChild(newFigCaption);
+
+  return newFigure;
+}
+
+// create a new item <div> for an individual product
+function createItemProduct(product)
+{
+  var newItem;
+  var newElm;
+
+  newItem = document.createElement("div");
+  newItem.setAttribute("class", "item")
+
+  newElm = createItemFigure(product);
+  newItem.appendChild(newElm);
+
+  newElm = document.createElement("p");
+  newElm.setAttribute("class", "item-description");
+  newElm.innerHTML = product.description;
+  newItem.appendChild(newElm);
+
+  newElm = document.createElement("p");
+  newElm.setAttribute("class", "item-price");
+  newElm.innerHTML = "$" + product.price.toFixed(2);
+  newItem.appendChild(newElm);
+
+  newElm = document.createElement("button");
+  newElm.setAttribute("onclick", "onClickAdd('" + product.name + "')");
+  newElm.innerHTML = "Add to Cart";
+  newItem.appendChild(newElm);
+
+  return newItem;
+}
+
+// update <div id="items"> from productList array
+function updateItemContainer(productList)
+{
+  var container = document.getElementById("items");
+
+  container.innerHTML = "";
+
+  if (productList.length == 0) {
+    container.innerHTML = "No items matching filter.";
+  } else {
+    for (product of productList) {
+      container.appendChild(createItemProduct(product));
+    }
+  }
+}
+
+// create products list as specified by category filter and sorting requirement
+function createProductList(itemSort, itemFilter) {
+  var productList;
+  var productListType;
+
+  // filter the products based on type, pageProductType defined in HTML <script> element
+  productListType = products;
+  if (typeof pageProductType != 'undefined') {
+    if (pageProductType.length != 0) {
+      productListType = products.filter(function(product) {
+        return product.productType == pageProductType;
+      });
+    }
+  }
+
+  // filter the products based on category
+  if (itemFilter.length == 0) {
+    productList = productListType;
+  } else {
+    productList = productListType.filter(function(product) {
+        return product.category == itemFilter;
+      });
+  }
+
+  // sort the products based on sorting specified by itemSort
+  switch (itemSort) {
+    case 'priceA':
+      productList = productList.sort(compareByPrice);
+      break;
+    case 'priceD':
+      productList = productList.sort(compareByPrice).reverse();
+      break;
+    case 'name':
+      productList = productList.sort(compareByName);
+      break;
+    default:
+      // do nothing
+      break;
+  }
+
+  return productList;
+}
+
+// update the list of products
+function updateProductList()
+{
+  var itemSort = '';
+  var itemFilter = '';
+
+  if (typeof document.filterForm != 'undefined') {
+    itemSort = document.filterForm.sortBy.value;
+    itemFilter = document.filterForm.filterBy.value;
+  }
+
+  // pageProduct
+  var prodList = createProductList(itemSort, itemFilter);
+
+  updateItemContainer(prodList);
+}
+
+// filter form's submit ("Update") button
+function captureFilter() {
+  updateProductList();
+  event.preventDefault();
+}
+
+
+
