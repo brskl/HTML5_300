6ffb610f14171485ea7ecc08bbc6ddd14162e816
brsklar <brskl@msn.com>
2017-06-11 19:54:46 -0700

    added logout function and button

--- hw09/js/user.js
+++ hw09/js/user.js
@@ -9,14 +9,26 @@ function loadUpdateUser() {
   }
 
   var userLink = document.getElementById("user");
+  var userLogout = document.getElementById("userLogout");
 
   var userStorage = sessionStorage["user"];
   if (userStorage) {
     user = JSON.parse(userStorage);
     userLink.innerHTML = user.name;
+    userLogout.style.display = "inline";
   } else {
     user = null;
     userLink.innerHTML = "Sign In";
+    userLogout.style.display = "none";
   }
+}
+
+function userLogout() {
+  if (!Modernizr.sessionstorage) {
+    return;
+  }
+
+  sessionStorage.removeItem("user");
+  loadUpdateUser();
 }
 
