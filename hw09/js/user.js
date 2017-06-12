f0c972426d9bffa9e1bfe26f5bbccf81f4b57b5f
brsklar <brskl@msn.com>
2017-06-11 17:20:44 -0700

    used jQuery to show/hide devs fleshed out formUserSignin

--- hw09/js/user.js
+++ hw09/js/user.js
@@ -33,7 +33,30 @@ function UpdateUserPage() {
     $('#userSignin').show()
     $('#userInfo').hide();
   } else {
-    userSignin.hide();
-    userInfo.show();
+    $('#userSignin').hide();
+    $('#userInfo').show();
+    // TODO: fill in userInfo form with data from user
   }
+}
+
+function onsubmitUserSignin(evt) {
+  evt.preventDefault();
+  
+  var userId = document.formUserSignin.userId.value;
+  var userPassword = document.formUserSignin.userPassword.value;
+
+  console.log("User/password: " + userId + " " + userPassword);
+  
+  // hard-code user information. TODO: Try via AWS Cognito
+  user = new Object;
+  user.name="Benjamin Sklar";
+  user.id="brskl";
+  user.email="brskl@msn.com";
+  user.address = new Object;
+  user.address.line1="1111 First Avt.";
+  user.address.city="Seattle";
+  user.address.state="WA";
+  user.address.zip="98105";
+  
+  sessionStorage["user"] = JSON.stringify(user);
 }
